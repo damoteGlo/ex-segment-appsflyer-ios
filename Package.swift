5a59896e072f7ea618e86250c37d8c165e0866d1
Dmote <dustin@yogaglo.com>
2022-05-11 12:45:51 -0700

    establish: update manifest to use Glo Apple Team's Fork

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,27 +6,23 @@ import PackageDescription
 let package = Package(
     name: "segment-appsflyer-ios",
     platforms: [
-        .iOS(.v10)
+        .iOS(.v15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "segment-appsflyer-ios",
             targets: ["segment-appsflyer-ios"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(name: "Segment", url: "https://github.com/segmentio/analytics-ios.git" , from: "4.0.0"),
-        .package(name: "AppsFlyerLib" , url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework.git", from: "6.5.2"),
+        .package(url: "https://github.com/segmentio/analytics-ios.git", from: "4.0.0"),
+        .package(url: "https://github.com/damoteGlo/ex-glo-apple-appsflyer.git", branch: "glo-develop")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "segment-appsflyer-ios",
             dependencies: [
-                "Segment",
-                .product(name: "AppsFlyerLib", package: "AppsFlyerLib"),
+                .product(name: "Segment", package: "analytics-ios"),
+                .product(name: "AppsFlyerLib", package: "ex-glo-apple-appsflyer"),
                 ],
             path: "segment-appsflyer-ios/Classes",
             publicHeadersPath: ""
